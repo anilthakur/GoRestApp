0ebffc88dc547da6409c37b93f44111d7893e950
anilkumar <anil.kumar4@tesco.com>
2020-03-09 08:59:38 +0530

    -- esperso test case

--- app/src/androidTest/java/com/anil/gorestapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/anil/gorestapp/MainActivityTest.kt
@@ -1,18 +1,18 @@
 package com.anil.gorestapp
 
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import com.anil.gorestapp.presentation.adapter.PersonAdapter
 import com.anil.gorestapp.presentation.view.MainActivity
 import junit.framework.Assert
-import org.hamcrest.Matchers
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,6 +25,7 @@ import org.junit.runner.RunWith
 class MainActivityTest {
     @get:Rule
     var activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         val appContext = InstrumentationRegistry.getInstrumentation().getTargetContext()
@@ -35,20 +36,23 @@ class MainActivityTest {
     @Test
     fun container_IsDisplayed() {
 
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerView))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
+        onView(withId(R.id.recyclerView)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withId(R.id.toolbar)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withText("asdasdsa")).check(doesNotExist())
     }
-
-
 
 
     @Test
     fun recyclerViewScrollToPositionTest() {
-        val recyclerView = activityRule.getActivity().findViewById<RecyclerView>(R.id.recyclerView)
-        val itemCount = recyclerView.getAdapter()!!.getItemCount()
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerView))
-                .inRoot(RootMatchers.withDecorView(Matchers.`is`(activityRule.activity.window.decorView)))
-                .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(itemCount))
+            tapRecyclerViewItem(R.id.recyclerView,15)
+
+
     }
+
+    fun tapRecyclerViewItem(recyclerViewId: Int, position: Int) {
+        onView(withId(recyclerViewId)).perform(scrollToPosition<PersonAdapter.ViewHolder>(position))
+    }
+
+
 }
